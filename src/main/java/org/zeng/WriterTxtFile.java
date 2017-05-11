35e8682a810d2f8a0db09b82cb48b55339b1b56e
anfree <zengxiangjiang@aliyun.com>
2017-05-12 00:12:05 +0800

    测试

--- src/main/java/org/zeng/WriterTxtFile.java
+++ src/main/java/org/zeng/WriterTxtFile.java
@@ -5,16 +5,20 @@ import java.io.*;
 public class WriterTxtFile {
 
     void writerTxt() {
+        System.out.println("------------- start --------------");
+        int payOrderNumber = 1200;
+        int proportion = 5;
+        int orderDetailNumber = payOrderNumber * proportion;
         BufferedWriter fw = null;
         try {
-            File file = new File("D:/RFXS_9994_20170509_USD_20170509A.txt");
+            File file = new File("D:/RFXS_70510002_20170511_USD_20170511A.txt");
             fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), "GBK")); // 指定编码格式，以免读取时中文字符异常
 
-            fw.append("9994|20170509A|20170509|1200|120000|USD|\n" +
-                    "Payment order start\n");
+            fw.append(String.format("70510002|20170511A|20170511|%s|%s|USD|\n", payOrderNumber, orderDetailNumber));
+            fw.append("Payment order start\n");
             int payOrderId = 1;
-            for (int i = 0; i < 1200; i++) {
-                fw.append(String.format("20170509N%s|0|440902198608270069|CCB|张莹|6217003110014723421|18219340978|USD|100||01122030\n", payOrderId++));
+            for (int i = 0; i < payOrderNumber; i++) {
+                fw.append(String.format("20170511N%s|0|123456789012345678|CCB|曾哥|1234567890123456789|18212345678|USD|0.05||01122030\n", payOrderId++));
             }
             fw.append("Payment order end");
             fw.newLine();
@@ -22,18 +26,15 @@ public class WriterTxtFile {
             fw.newLine();
             payOrderId = 1;
             int orderId = 1;
-            for (int i = 0; i < 120000; i++) {
-                if (i != 0 && i % 100 == 0) {
-                    fw.append(String.format("20170509N%s|%s|20160718|204125|20160202|204206|USD|1.00|Baby Shampoo|1\n", payOrderId, orderId++));
-                    payOrderId++;
+            for (int i = 1; i < orderDetailNumber+1; i++) {
+                if (i % proportion != 0) {
+                    fw.append(String.format("20170511N%s|%s|20160718|204125|20160202|204206|USD|0.01|Baby Shampoo|1\n", payOrderId, orderId++));
                 } else {
-                    fw.append(String.format("20170509N%s|%s|20160718|204125|20160202|204206|USD|1.00|Baby Shampoo|1\n", payOrderId, orderId++));
+                    fw.append(String.format("20170511N%s|%s|20160718|204125|20160202|204206|USD|0.01|Baby Shampoo|1\n", payOrderId++, orderId++));
                 }
             }
             fw.append("Order detail end");
-            System.out.println("---------------------------");
             System.out.println("file: %s write finished");
-            System.out.println("---------------------------");
             fw.flush(); // 全部写入缓存中的内容
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,6 +46,7 @@ public class WriterTxtFile {
                     e.printStackTrace();
                 }
             }
+            System.out.println("-------------- end -------------");
         }
     }
 
